b0f1f1ede9716c2364bc48d413235f27d3753422
André <andre.azevedo@concrete.com.br>
2018-12-14 12:52:20 -0200

    Primeira Versão

---
+++ features/get.feature
@@ -0,0 +1,7 @@
+#language: pt
+Funcionalidade: Utilizar o GET para retornar uma lista
+
+Cenario: Retornar uma lista através da requisição GET
+  Dado que acesso um endpoint
+  Quando realizar uma requisição GET
+  Entao retorna a lista
---
+++ features/step_definitions/get.rb
@@ -0,0 +1,18 @@
+#language: pt
+
+require 'httparty'
+  
+  Dado("que acesso um endpoint") do
+    @endereco = 'http://localhost:3000/usuarios/1'
+  end
+  
+  Quando("realizar uma requisição GET") do
+    @retorno = HTTParty.get @endereco
+  end
+  
+  Entao("retorna a lista") do
+    @retorno.body
+    puts @retorno.body
+    #puts "Nome: #{@retorno.nome}"
+    #puts "Location: #{retorno.location}"
+  end
